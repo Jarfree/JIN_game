b4b33e7ea13a98ed5b662c9eb80087a35f5ca825
Jeffrey <65458453+Jarfree@users.noreply.github.com>
2024-08-29 20:44:55 -0500

    added gitkeeps

--- Menus/menus.gd
+++ Menus/menus.gd
@@ -7,11 +7,22 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+		pass
+		
+		
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		if self.is_visible_in_tree() :
+			print('escape pressed, game unpaused')
+			self.show()
+			get_tree().paused = true
+		else : 
+			print('escape pressed, game paused')
+			self.show()
+			get_tree().paused = true
 
 func _on_button_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://[drag scene here].tscn")
+	get_tree().change_scene_to_file("res://Levels/Temp.tscn")
 
 
 func _on_button_settings_pressed() -> void:
@@ -20,3 +31,8 @@ func _on_button_settings_pressed() -> void:
 
 func _on_button_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_button_resume_pressed() -> void:
+	self.hide()
+	#get_tree().paused = false
